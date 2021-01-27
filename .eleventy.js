0a09d556e2d23153711c98ade3d75f90e9bf750c
Sean McPherson <git@seanmcp.com>
2021-01-27 07:06:25 -0500

    feat: add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 module.exports = (config) => {
+  /* ---------- PLUGINS ---------- */
   config.addPlugin(require("@11ty/eleventy-plugin-rss"));
-  // Settings
-  config.addPassthroughCopy({ "src/_assets/": "assets/" });
-  config.setDataDeepMerge(true);
 
-  // Filters
+  /* ---------- SETTINGS ---------- */
+  config.addPassthroughCopy({ "src/_assets/": "assets/" });
+  // Reload on sass changes
+  config.addWatchTarget("src/_theme/sass/")
+  config.setDataDeepMerge(true);
+  
+  /* ---------- FILTERS ---------- */
   config.addFilter("getDateFromISO", (date) => {
     return date.toISOString().split("T")[0].replace(/-/g, "/");
   });
