a2d17a74d1d032ab036cf7121fc6c2683632f0f2
Sean McPherson <git@seanmcp.com>
2021-01-30 14:32:57 -0500

    feat: add eleventy-img plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,23 @@
 module.exports = (config) => {
   /* ---------- PLUGINS ---------- */
   config.addPlugin(require("@11ty/eleventy-plugin-rss"));
-  config.addPlugin(require('./src/_plugins/inline'))
-  config.addPlugin(require('./src/_plugins/404'))
+  config.addPlugin(require("./src/_plugins/inline"));
+  config.addPlugin(require("./src/_plugins/404"));
+  config.addPlugin(require("./src/_plugins/image"));
 
   /* ---------- SETTINGS ---------- */
   config.addPassthroughCopy({ "src/_assets/": "assets/" });
   // Reload on sass changes
-  config.addWatchTarget("src/_theme/sass/")
+  config.addWatchTarget("src/_theme/sass/");
   config.setDataDeepMerge(true);
-  
+
   /* ---------- FILTERS ---------- */
   config.addFilter("dateToSlug", (date) => {
     return date.toISOString().split("T")[0].replace(/-/g, "/");
   });
 
   return {
+    markdownTemplateEngine: 'njk',
     dir: {
       input: "src/",
     },
---
+++ src/_plugins/image.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const Image = require("@11ty/eleventy-img");
+
+const urlPattern = /^https?:\/\//;
+
+function buildFilename(src, width, format = 'jpeg') {
+    const extension = path.extname(src);
+    const name = path.basename(src, extension);
+    return `${name}-${width}w.${format}`;
+}
+
+module.exports = function (config) {
+  config.addFilter("imageUrl", (src, width = 480) => {
+    return `/assets/images/${buildFilename(src, width)}`;
+  });
+
+  config.addNunjucksAsyncShortcode(
+    "image",
+    async function (src, alt, contextClass, sizes) {
+      let metadata = await Image(
+        urlPattern.test(src) ? src : "src/_assets/images/" + src,
+        {
+          filenameFormat: function (_, src, width, format) {
+            return buildFilename(src, width, format);
+          },
+          outputDir: "./_site/assets/images/",
+          urlPath: "/assets/images/",
+          widths: [480, 960, 1200],
+        }
+      );
+
+      let imageAttributes = {
+        alt,
+        class: `eleventy-img${contextClass ? ` ${contextClass}` : ""}`,
+        decoding: "async",
+        loading: "lazy",
+        sizes,
+      };
+
+      return Image.generateHTML(metadata, imageAttributes);
+    }
+  );
+};
